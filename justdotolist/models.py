0ab2a483651b654526f701df092642066e47aad0
smiller28 <s1.m1ll3r@hotmail.com>
2020-10-27 21:52:23 +0000

    Added Author to models.py and created forms.py

--- justdotolist/models.py
+++ justdotolist/models.py
@@ -21,6 +21,7 @@ class BlogPost(models.Model):
 class Author(models.Model):
     first_name = models.CharField(max_length=50)
     last_name = models.CharField(max_length=50)
+    password = models.CharField(max_length=20)
     email = models.EmailField()
     author_alias = models.CharField(max_length=100)
     created_date = models.DateTimeField()
