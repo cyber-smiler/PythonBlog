9be124e888bd9a4d0f5e4df5bb6bca5accecd42c
smiller28 <s1.m1ll3r@hotmail.com>
2020-10-27 21:47:30 +0000

    First Commit for mysite

--- justdotolist/models.py
+++ justdotolist/models.py
@@ -17,3 +17,10 @@ class BlogPost(models.Model):
     def __str__(self):
         return self.title
 
+
+class Author(models.Model):
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    email = models.EmailField()
+    author_alias = models.CharField(max_length=100)
+    created_date = models.DateTimeField()
